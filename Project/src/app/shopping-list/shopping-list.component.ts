394fa0d1c79e3a8959e3f8e103e1bb22adff6649
unknown <sipple3@gmail.com>
2019-04-09 17:34:08 -0400

    added basic shopping html parts

--- Project/src/app/shopping-list/shopping-list.component.ts
+++ Project/src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model'
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5, 'ea'),
+    new Ingredient('Oranges', 2, 'ea')
+  ];
 
-  constructor() { }
+  constructor() { console.log(this.ingredients)}
 
   ngOnInit() {
   }
