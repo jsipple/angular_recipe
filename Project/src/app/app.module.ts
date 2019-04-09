9429cef9287fc80cd3eed3b16f147d2326357d96
unknown <sipple3@gmail.com>
2019-04-09 14:53:51 -0400

    added basic file having a issue with grabbing the recipe component so need to look into that

---
+++ Project/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+// this is not working for some reason
+// import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeListComponent } from './Recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './Recipe/recipe-detail/recipe-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    // RecipeComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
